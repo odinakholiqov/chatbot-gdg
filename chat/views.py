a05bd658d245e1ba7a30d04e2a41cb0a9507f406
Odina Kholiqov <odinakholiqov@outlook.com>
2024-08-26 00:34:10 +0500

    update

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,68 @@
+import uuid
+
+from django.views.decorators.http import require_http_methods
+from django.views.decorators.csrf import csrf_exempt
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from google.cloud.dialogflow_v2.services.agents import AgentsClient
+from google.cloud.dialogflow_v2.services.sessions import SessionsClient
+from google.cloud.dialogflow_v2.types import TextInput, QueryInput, DetectIntentRequest
+
+@require_http_methods(['GET'])
+def index_view(request):
+    return render(request, 'home.html')
+
+
+@csrf_exempt
+@require_http_methods(['POST'])
+def chat_view(request):
+    print('Body', request.body)
+
+    project_id = "gdg-workshop-chatbot"
+    # For more information about regionalization see https://cloud.google.com/dialogflow/cx/docs/how/region
+    location_id = "asia-south1"
+    # For more info on agents see https://cloud.google.com/dialogflow/cx/docs/concept/agent
+    agent_id = "eba504e3-01cd-4974-8664-5338ced33c70"
+    agent = f"projects/{project_id}/locations/{location_id}/agents/{agent_id}"
+    # For more information on sessions see https://cloud.google.com/dialogflow/cx/docs/concept/session
+    session_id = uuid.uuid4()
+    texts = ["Hello"]
+    # For more supported languages see https://cloud.google.com/dialogflow/es/docs/reference/language
+    language_code = "en-us"
+
+    detect_intent_texts(agent, session_id, texts, language_code)
+
+    return HttpResponse("response.query_result.fulfillment_text", status=200)
+
+def detect_intent_texts(agent, session_id, texts, language_code):
+    """Returns the result of detect intent with texts as inputs.
+
+    Using the same `session_id` between requests allows continuation
+    of the conversation."""
+    session_path = f"{agent}/sessions/{session_id}"
+    print(f"Session path: {session_path}\n")
+    client_options = None
+    agent_components = AgentsClient.parse_agent_path(agent)
+    location_id = agent_components["location"]
+    if location_id != "global":
+        api_endpoint = f"{location_id}-dialogflow.googleapis.com:443"
+        print(f"API Endpoint: {api_endpoint}\n")
+        client_options = {"api_endpoint": api_endpoint}
+    session_client = SessionsClient(client_options=client_options)
+
+    for text in texts:
+        text_input = TextInput(text=text)
+        query_input = QueryInput(text=text_input, language_code=language_code)
+        request = DetectIntentRequest(
+            session=session_path, query_input=query_input
+        )
+        response = session_client.detect_intent(request=request)
+
+        print("=" * 20)
+        print(f"Query text: {response.query_result.text}")
+        response_messages = [
+            " ".join(msg.text.text) for msg in response.query_result.response_messages
+        ]
+        print(f"Response text: {' '.join(response_messages)}\n")
